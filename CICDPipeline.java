517d44bf10f364d0570846c8a6df84c153de2a89
Rahul Chouhan <rahul7117chouhan@gmail.com>
2025-08-06 23:44:24 +0530

    Little Changes

--- CICDPipeline.java
+++ CICDPipeline.java
@@ -1,7 +1,6 @@
 public class CICDPipeline {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
-        System.out.println("Hello from Rahul Chouhan!");
+        System.out.println("Let's Begin the journey of Devops!");
     }
 }
 
